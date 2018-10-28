ace0e6ae471336023c357d6565b1946df3ceef19
Joe Deatrick <jldeatrick@protonmail.com>
2018-10-28 16:02:35 -0700

    [cleanup] forgot to git rm old folders

--- google/sample.js
+++
@@ -1,6 +0,0 @@
-chrome.storage.local.get("audio", function(result) {
-  var data = result["audio"];
-  var index = Math.floor(Math.random() * data.length);
-  document.getElementById("audio").src = data[index][24][0];
-  document.getElementById("transcript").innerHTML = data[index][9][0];
-});
