4ff9b20aa958efa7e63f2be069b031b9b252656a
Joe Deatrick <jldeatrick@protonmail.com>
2018-10-29 10:15:03 -0700

    [alexa] CSRF failure rate is better

---
+++ extension/choose.js
@@ -0,0 +1,16 @@
+document.getElementById("alexa").addEventListener("click", load_alexa);
+document.getElementById("google").addEventListener("click", load_google);
+
+function load_alexa(){
+	console.log("alexa!!")
+	chrome.tabs.update({
+	     url: "audio.html?device=alexa"
+	});
+}
+
+function load_google(){
+	window.location.href = "audio.html"
+	chrome.tabs.update({
+	     url: "audio.html?device=alexa"
+	});
+}
