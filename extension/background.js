abd8041cebf7099d9fba0bf203f388d654e84120
Joe Deatrick <jldeatrick@protonmail.com>
2018-11-09 17:37:04 -0800

    basic message sending and receiving

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,30 @@
+let alexa;
+let home;
+
+var manage_messages = function manage_messages(request, sender, sendResponse) {
+    console.log(request);
+    if (request === 'test') {
+        console.log('success!!');
+        sendResponse('okay');
+    } else if (request === 'alexa') {
+        /* do something */
+        console.log('wow!');
+        alexa = true;
+        home = false;
+    } else if (request === 'home') {
+        /* do something */
+        home = true;
+        alexa = false;
+    }
+};
+
+chrome.runtime.onMessageExternal.addListener(manage_messages);
+
 function buttonClicked() {
+    alexa = false;
+    home = false;
     chrome.tabs.create({
-        url: '/choose.html'
+        url: 'https://berkeley.qualtrics.com/jfe/form/SV_7NzNJ4QmCe4uE05'
     });
 }
 
