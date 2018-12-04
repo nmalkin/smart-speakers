09359dafc67777ce21efda9d40a4356781707c1d
Nathan Malkin <nmalkin@cs.berkeley.edu>
2018-12-03 23:00:21 -0800

    Consolidate webpack builds

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,11 @@ const BUILD_PATH = path.resolve(__dirname, 'chrome_extension');
 
 module.exports = [
     {
-        entry: './src/chrome/background/background.ts',
+        entry: {
+            background: './src/chrome/background/background.ts',
+            content: './src/chrome/content/content.ts',
+            options: './src/chrome/content/options.ts'
+        },
         module: {
             rules: [
                 {
@@ -20,7 +24,7 @@ module.exports = [
             extensions: ['.ts', '.js']
         },
         output: {
-            filename: 'background.js',
+            filename: '[name].js',
             path: BUILD_PATH
         },
         mode: 'none',
@@ -33,53 +37,7 @@ module.exports = [
                 {
                     from: 'assets/*',
                     to: ''
-                }
-            ])
-        ]
-    },
-    {
-        entry: './src/chrome/content/content.ts',
-        module: {
-            rules: [
-                {
-                    test: /\.ts$/,
-                    loader: 'ts-loader',
-                    options: { onlyCompileBundledFiles: true },
-                    exclude: /node_modules/
-                }
-            ]
-        },
-        resolve: {
-            extensions: ['.ts', '.js']
-        },
-        output: {
-            filename: 'content.js',
-            path: BUILD_PATH
-        },
-        mode: 'none'
-    },
-    {
-        entry: './src/chrome/content/options.ts',
-        module: {
-            rules: [
-                {
-                    test: /\.ts$/,
-                    loader: 'ts-loader',
-                    options: { onlyCompileBundledFiles: true },
-                    exclude: /node_modules/
-                }
-            ]
-        },
-        resolve: {
-            extensions: ['.ts', '.js']
-        },
-        output: {
-            filename: 'options.js',
-            path: BUILD_PATH
-        },
-        mode: 'none',
-        plugins: [
-            new CopyWebpackPlugin([
+                },
                 {
                     from: 'src/chrome/options.html',
                     to: ''
