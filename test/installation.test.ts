478bd7bdc7c9af030708aa55061fe64c9c7a0e12
Nathan Malkin <nmalkin@cs.berkeley.edu>
2018-12-08 18:58:37 -0800

    Share browser initialization code

--- test/installation.test.ts
+++ test/installation.test.ts
@@ -1,19 +1,7 @@
-import { join } from 'path';
-import * as puppeteer from 'puppeteer';
-
-const pathToExtension = join(__dirname, '../chrome_extension');
-console.log(pathToExtension);
+import { getBrowser } from './browser';
 
 test('the extension can be installed', async () => {
-    const options = {
-        headless: false,
-        ignoreHTTPSErrors: true,
-        args: [
-            `--disable-extensions-except=${pathToExtension}`,
-            `--load-extension=${pathToExtension}`
-        ]
-    };
-    const browser = await puppeteer.launch(options);
+    const browser = await getBrowser();
     expect(browser).toBeTruthy();
     await browser.close();
 }, 3000);
