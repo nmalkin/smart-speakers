9562130a75518df20735353b4919f69d4a96b343
Nathan Malkin <nmalkin@cs.berkeley.edu>
2018-12-21 09:02:29 -0800

    Subdivide Amazon diagnostics
    Refactoring the Amazon code in the process
    and adding some tests

--- src/common/alexa/amazon.ts
+++ src/common/alexa/amazon.ts
@@ -1,4 +1,8 @@
-import { ValidationResult, VerificationState } from '../../common/types';
+import {
+    ValidationResult,
+    VerificationState,
+    Interaction
+} from '../../common/types';
 
 const csrfReg = /csrfToken = "(.*)"/g;
 const expReg = /<audio id="audio-(.*)"> <source[\w\W]*?<div class="summaryCss">\s*(.*?)\s*<\/div/g;
@@ -17,69 +21,131 @@ function matchCSRF(pageText: string): string | null {
 }
 
 /**
+ * Return true if the audio id is well-formed
+ *
+ * A well-formed audio ID is expected to have a / as its next-to-last (104th) character.
+ * TODO: why? Is there anything more stable we can check for?
+ */
+function validAudioID(id: string): boolean {
+    return id.length >= 104 && id[104] === '/';
+}
+
+/**
+ * Given a match object returned by expReg, extracts the resulting interaction
+ *
+ * @see expReg
+ */
+function getInteractionFromMatch(match: RegExpMatchArray): Interaction | null {
+    if (match.length < 3) {
+        return null;
+    }
+
+    const audioID = match[1];
+    if (validAudioID(audioID)) {
+        const transcript = match[2];
+        const url = `https://www.amazon.com/hz/mycd/playOption?id=${audioID}`;
+        return { url, transcript };
+    } else {
+        return null;
+    }
+}
+
+/**
  * Extract interaction data from page HTML
+ *
+ * @param pageText the raw HTML of the page
+ * @returns an object with keys as the URL and transcripts as value, or an empty object if those could not be found
+ */
+export function extractAudio(pageText: string): Interaction[] {
+    const interactions: Interaction[] = [];
+    let match = expReg.exec(pageText);
+    while (match) {
+        const interaction = getInteractionFromMatch(match);
+        if (interaction) {
+            interactions.push(interaction);
+        }
+        match = expReg.exec(pageText);
+    }
+    return interactions;
+}
+
+/**
+ * Extract interaction data from page HTML
+ *
+ * @deprecated TODO: use extractAudio instead
  *
  * @param pageText the raw HTML of the page
  * @returns an object with keys as the URL and transcripts as value, or an empty object if those could not be found
  */
 function matchAudio(pageText: string): object {
     const dict = {};
-    let match = expReg.exec(pageText);
-    while (match) {
-        /* prune malformed ids. May want to revisit which of these are still accessible */
-        if (match[0][121] === '/') {
-            const transcript = match[2];
-            const url = `https://www.amazon.com/hz/mycd/playOption?id=${
-                match[1]
-            }`;
-            dict[url] = transcript;
-        }
-        match = expReg.exec(pageText);
-    }
-    console.log(dict);
+    const interactions = extractAudio(pageText);
+    interactions.forEach(interaction => {
+        dict[interaction.url] = interaction.transcript;
+    });
     return dict;
+}
+
+/**
+ * Get the contents of the overview page
+ *
+ * We use it only to extract the CSRF token.
+ */
+async function getCSRFPage(): Promise<string> {
+    const response = await fetch(
+        'https://www.amazon.com/hz/mycd/myx#/home/content/booksAll/dateDsc/'
+    );
+    const text = await response.text();
+    return text;
 }
 
 /**
  * Fetch the overview page
  */
-function getCSRF() {
-    return fetch(
-        'https://www.amazon.com/hz/mycd/myx#/home/content/booksAll/dateDsc/'
-    )
-        .then(response => response.text())
-        .then(text => matchCSRF(text))
-        .catch(err => {
-            console.log(err);
-            return null;
-        });
+async function getCSRF(): Promise<string | null> {
+    try {
+        const text = await getCSRFPage();
+        return matchCSRF(text);
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+/**
+ * Get the page with activity data and return its contents
+ * @param token the CSRF token to use in the request
+ */
+async function fetchTranscriptPage(token: string): Promise<string> {
+    const response = await fetch(
+        'https://www.amazon.com/hz/mycd/alexa/activityTranscripts',
+        {
+            method: 'POST',
+            body: `csrfToken=${token}&rangeType=custom&startDate=000000000000&endDate=9999999999999&batchSize=999999&shouldParseStartDate=false&shouldParseEndDate=false`,
+            mode: 'cors',
+            redirect: 'follow',
+            headers: new Headers({
+                'Content-Type': 'application/x-www-form-urlencoded'
+            })
+        }
+    );
+    const text = await response.text();
+    return text;
 }
 
 /**
  * Get the page with activity data and return the parsed interaction data
  * @param tok the CSRF token to use in the request
  */
-function getAudio(tok: string) {
-    /* make the AJAX request for the activity transcripts */
-    return fetch('https://www.amazon.com/hz/mycd/alexa/activityTranscripts', {
-        method: 'POST',
-        body: `csrfToken=${tok}&rangeType=custom&startDate=000000000000&endDate=9999999999999&batchSize=999999&shouldParseStartDate=false&shouldParseEndDate=false`,
-        mode: 'cors',
-        redirect: 'follow',
-        headers: new Headers({
-            'Content-Type': 'application/x-www-form-urlencoded'
-        })
-    })
-        .then(response => response.text())
-        .then(text => {
-            if (
-                text !==
-                '{"ERROR":"{\\"success\\":false,\\"error\\":\\"CSRF_VALIDATION_FAILED\\"}"}'
-            ) {
-                return matchAudio(text);
-            }
-            return null;
-        });
+async function getAudio(tok: string): Promise<object | null> {
+    const text = await fetchTranscriptPage(tok);
+    if (
+        text !==
+        '{"ERROR":"{\\"success\\":false,\\"error\\":\\"CSRF_VALIDATION_FAILED\\"}"}'
+    ) {
+        return matchAudio(text);
+    }
+    return null;
 }
 
 /**
@@ -109,4 +175,13 @@ async function validateAmazon(): Promise<ValidationResult> {
     }
 }
 
-export { getCSRF, matchCSRF, matchAudio, getAudio, validateAmazon };
+export {
+    validAudioID,
+    getInteractionFromMatch,
+    getCSRF,
+    getCSRFPage,
+    matchCSRF,
+    fetchTranscriptPage,
+    matchAudio,
+    validateAmazon
+};
--- test/amazon.test.ts
+++ test/amazon.test.ts
@@ -1,4 +1,10 @@
-import { matchCSRF, matchAudio } from '../src/common/alexa/amazon';
+import {
+    matchCSRF,
+    matchAudio,
+    validAudioID,
+    getInteractionFromMatch,
+    extractAudio
+} from '../src/common/alexa/amazon';
 
 const sampleCSRF =
     'var isDesktop = "true"; ' +
@@ -15,19 +21,126 @@ const sampleTranscript =
     '                          “when is kingdom hearts three coming out”</div>\n' +
     '                      </span>\n';
 
-test('finds and encodes csrf token', () => {
-    expect.assertions(1);
-    const token =
-        'gMEhI0aNH1xlXvtJF%2FwsG4uemtItdFMJBHDp9xYAAAAJAAAAAFvfRMJyYXcAAAAA';
-    expect(matchCSRF(sampleCSRF)).toEqual(token);
+describe('matchCSRF', () => {
+    test('finds and encodes csrf token', () => {
+        expect.assertions(1);
+        const token =
+            'gMEhI0aNH1xlXvtJF%2FwsG4uemtItdFMJBHDp9xYAAAAJAAAAAFvfRMJyYXcAAAAA';
+        expect(matchCSRF(sampleCSRF)).toEqual(token);
+    });
+
+    test("it returns null if it can't find the token", () => {
+        const page =
+            'var isDesktop = "true"; ' +
+            '				var someOtherToken = "gMEhI0aNH1xlXvtJF/wsG4uemtItdFMJBHDp9xYAAAAJAAAAAFvfRMJyYXcAAAAA"; ' +
+            '		        var kindleAppsSupported = false; ' +
+            '		        var myxWebsiteConfig = {};';
+        expect(matchCSRF(page)).toBeNull();
+    });
 });
 
-test('displays the correct transcript', () => {
-    const transcript =
-        'A3S5BH2HU6VAYF:1.0/2018/10/13/20/G090LF1181840BFC/57:10::TNIH_2V.a9baef64-be15-4776-8e84-f1830509730bZXV/1';
-    const dict = matchAudio(sampleTranscript);
-    expect(Object.keys(dict).length).toEqual(1);
-    expect(
-        dict[`https://www.amazon.com/hz/mycd/playOption?id=${transcript}`]
-    ).toEqual('“when is kingdom hearts three coming out”');
+describe('validAudioID', () => {
+    test('it matches a valid ID', () => {
+        expect(
+            validAudioID(
+                'A3S5BH2HU6VAYF:1.0/2018/10/13/20/G090LF1181840BFC/57:10::TNIH_2V.a9baef64-be15-4776-8e84-f1830509730bZXV/1'
+            )
+        ).toBeTruthy();
+    });
+
+    test('it rejects an invalid ID', () => {
+        expect(
+            validAudioID(
+                '_A3S5BH2HU6VAYF:1.0/2018/10/13/20/G090LF1181840BFC/57:10::TNIH_2V.a9baef64-be15-4776-8e84-f1830509730bZXV/1'
+            )
+        ).toBeFalsy();
+    });
+});
+
+describe('getInteractionFromMatch', () => {
+    test('it returns an interaction', () => {
+        const id =
+            'A3S5BH2HU6VAYF:1.0/2018/10/13/20/G090LF1181840BFC/57:10::TNIH_2V.a9baef64-be15-4776-8e84-f1830509730bZXV/1';
+        expect(
+            getInteractionFromMatch(['matched text', id, 'transcript'])
+        ).toEqual({
+            url:
+                'https://www.amazon.com/hz/mycd/playOption?id=A3S5BH2HU6VAYF:1.0/2018/10/13/20/G090LF1181840BFC/57:10::TNIH_2V.a9baef64-be15-4776-8e84-f1830509730bZXV/1',
+            transcript: 'transcript'
+        });
+    });
+
+    test('it returns null if audio id is invalid', () => {
+        const id =
+            '!A3S5BH2HU6VAYF:1.0/2018/10/13/20/G090LF1181840BFC/57:10::TNIH_2V.a9baef64-be15-4776-8e84-f1830509730bZXV/1';
+        expect(
+            getInteractionFromMatch(['matched text', id, 'transcript'])
+        ).toBeNull();
+    });
+
+    test('it returns null if not enough components matched', () => {
+        const id = expect(
+            getInteractionFromMatch(['missing', 'matches'])
+        ).toBeNull();
+    });
+});
+
+describe('extractAudio', () => {
+    test('returns the correct transcript', () => {
+        const id =
+            'A3S5BH2HU6VAYF:1.0/2018/10/13/20/G090LF1181840BFC/57:10::TNIH_2V.a9baef64-be15-4776-8e84-f1830509730bZXV/1';
+        const result = extractAudio(sampleTranscript);
+        expect(result[0]).toEqual({
+            url: `https://www.amazon.com/hz/mycd/playOption?id=${id}`,
+            transcript: '“when is kingdom hearts three coming out”'
+        });
+    });
+
+    test('skips action if audio ID is malformed', () => {
+        const page =
+            '<audio id="audio-A3S5BH2HU6VAYF::1.0/2018/10/13/20/G090LF1181840BFC/57:10::TNIH_2V.a9baef64-be15-4776-8e84-f1830509730bZXV/1"> <source id="audioSource-A3S5BH2HU6VAYF::1.0/2018/10/13/20/G090LF1181840BFC/57:10::TNIH_2V.a9baef64-be15-4776-8e84-f1830509730bZXV/1"></audio>\n' +
+            '                     <span class="playButton" id="playIcon-50" attr="A3S5BH2HU6VAYF:1.0/2018/10/13/20/G090LF1181840BFC/57:10::TNIH_2V.a9baef64-be15-4776-8e84-f1830509730bZXV/1" onclick="playOption(\'A3S5BH2HU6VAYF:1.0/2018/10/13/20/G090LF1181840BFC/57:10::TNIH_2V.a9baef64-be15-4776-8e84-f1830509730bZXV/1\', 50)">\n' +
+            '                     <img id="playOptionIcon-50" style="width: 20px;" src = "https://m.media-amazon.com/images/G/01/digital/fiona/myx/ic_play_normal._CB1531740338_.png" />\n' +
+            '                     </span>\n' +
+            '                    <div class="summaryCss">\n' +
+            '                          “when is kingdom hearts three coming out”</div>\n' +
+            '                      </span>\n';
+
+        const result = extractAudio(page);
+        expect(result).toEqual([]);
+    });
+
+    test('returns empty dict if nothing is found', () => {
+        expect(matchAudio('a test string without audio')).toEqual({});
+    });
+});
+
+describe('matchAudio', () => {
+    test('displays the correct transcript', () => {
+        const transcript =
+            'A3S5BH2HU6VAYF:1.0/2018/10/13/20/G090LF1181840BFC/57:10::TNIH_2V.a9baef64-be15-4776-8e84-f1830509730bZXV/1';
+        const dict = matchAudio(sampleTranscript);
+        expect(Object.keys(dict).length).toEqual(1);
+        expect(
+            dict[`https://www.amazon.com/hz/mycd/playOption?id=${transcript}`]
+        ).toEqual('“when is kingdom hearts three coming out”');
+    });
+
+    test('skips action if audio ID is malformed', () => {
+        const page =
+            '<audio id="audio-A3S5BH2HU6VAYF::1.0/2018/10/13/20/G090LF1181840BFC/57:10::TNIH_2V.a9baef64-be15-4776-8e84-f1830509730bZXV/1"> <source id="audioSource-A3S5BH2HU6VAYF::1.0/2018/10/13/20/G090LF1181840BFC/57:10::TNIH_2V.a9baef64-be15-4776-8e84-f1830509730bZXV/1"></audio>\n' +
+            '                     <span class="playButton" id="playIcon-50" attr="A3S5BH2HU6VAYF:1.0/2018/10/13/20/G090LF1181840BFC/57:10::TNIH_2V.a9baef64-be15-4776-8e84-f1830509730bZXV/1" onclick="playOption(\'A3S5BH2HU6VAYF:1.0/2018/10/13/20/G090LF1181840BFC/57:10::TNIH_2V.a9baef64-be15-4776-8e84-f1830509730bZXV/1\', 50)">\n' +
+            '                     <img id="playOptionIcon-50" style="width: 20px;" src = "https://m.media-amazon.com/images/G/01/digital/fiona/myx/ic_play_normal._CB1531740338_.png" />\n' +
+            '                     </span>\n' +
+            '                    <div class="summaryCss">\n' +
+            '                          “when is kingdom hearts three coming out”</div>\n' +
+            '                      </span>\n';
+
+        const dict = matchAudio(page);
+        expect(dict).toEqual({});
+    });
+
+    test('returns empty dict if nothing is found', () => {
+        expect(matchAudio('a test string without audio')).toEqual({});
+    });
 });
