8cc99239b2a1bbf575ec879ae3dd647eecc0c774
Nathan Malkin <nmalkin@cs.berkeley.edu>
2019-01-16 16:44:36 -0800

    Move eligibility check to its own file

--- src/chrome/content/content.ts
+++ src/chrome/content/content.ts
@@ -14,6 +14,7 @@ import {
 } from './views';
 import { selectUnseen, zip } from '../../common/util';
 import { reportError, initErrorHandling } from '../common/errors';
+import { checkEligibility } from '../../common/eligibility';
 
 class SurveyState {
     public device: Device;
@@ -102,40 +103,6 @@ async function processRecordingRequest(
 
     // Display recording on page
     displayInteraction(interaction, questionNumber);
-}
-
-/**
- * Check the current user's eligibility for the study.
- * If validation failed, then that status is preserved.
- * If validation succeeded but the user is ineligible, their status is updated.
- */
-function checkEligibility(result: ValidationResult): void {
-    if (result.status !== VerificationState.loggedIn) {
-        // Validation failed, so we keep the status as-is.
-        // Then the appropriate error message can be displayed to the user.
-        return;
-    }
-
-    // Check eligibility
-    // A user is eligible if they have:
-    // 1) At least 30 recordings
-    if (!result.interactions || result.interactions.length < 30) {
-        result.status = VerificationState.ineligible;
-        return;
-    }
-
-    // 2) Had the device for at least 30 days
-    const oldestInteraction = result.interactions
-        .map(interaction => interaction.timestamp)
-        .sort()[0];
-    const elapsedMilliseconds = Date.now() - oldestInteraction;
-    const elapsedDays = elapsedMilliseconds / (1000 * 60 * 60 * 24);
-    if (elapsedDays < 30) {
-        result.status = VerificationState.ineligible;
-        return;
-    }
-
-    // Good news! Validation succeeded.
 }
 
 /**
