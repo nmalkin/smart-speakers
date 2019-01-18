nmalkin/smart-speakers

commit 1aa529a76dde952635b7b49ccd55cf34576e7115

Move data fetching & validation into background script
Chrome is shifting to disallow cross-origin fetches in content scripts,
even if the extension has the right permissions for it.
See #67 and https://www.chromium.org/Home/chromium-security/extension-content-script-fetches

This commit addresses that by moving the validation steps
(which include the cross-origin requests we make) into the background script.

src/common/google/google.ts
@@ -392,6 +392,7 @@ async function validateGoogle(): Promise<ValidationResult> {
 }
 
 export const Google: Device = {
+    serialize: () => 'google',
     accountName: 'Google',
     loginURL: 'https://accounts.google.com/ServiceLogin',
     validate: validateGoogle

src/chrome/background/background.ts
@@ -1,5 +1,35 @@
 import { updateDevEnvironmentStatus } from '../common/debug';
-import { initErrorHandling } from '../common/errors';
+import { initErrorHandling, reportError, reportIssue } from '../common/errors';
+import { ValidationResult, VerificationState } from '../../common/types';
+import { Device, unserializeDevice } from '../../common/device';
+import { checkEligibility } from '../../common/eligibility';
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    /** Process validation requests */
+    if (request.type && request.type === 'validate') {
+        const device: Device = unserializeDevice(request.device);
+
+        device
+            .validate()
+            .then((result: ValidationResult) => {
+                checkEligibility(result);
+                sendResponse(result);
+            })
+            .catch(error => {
+                reportError(error);
+                sendResponse({ status: VerificationState.error });
+            });
+    } else {
+        reportIssue(
+            `received unexpected message from content-script: ${request}`
+        );
+        sendResponse({ status: VerificationState.error });
+    }
+
+    // Returning true flags that we want to use sendResponse asynchronosuly
+    // see https://developer.chrome.com/extensions/runtime#event-onMessage
+    return true;
+});
 
 function openStartPage() {
     chrome.tabs.create({

src/chrome/content/content.ts
@@ -106,6 +106,20 @@ async function processRecordingRequest(
 }
 
 /**
+ * Tell the background script to perform validation, then return the result
+ */
+async function validate(device: Device): Promise<ValidationResult> {
+    return new Promise((resolve, reject) => {
+        chrome.runtime.sendMessage(
+            { type: 'validate', device: device.serialize() },
+            response => {
+                resolve(response);
+            }
+        );
+    });
+}
+
+/**
  * Process 'verify' message
  */
 async function processVerify(state: SurveyState) {
@@ -113,8 +127,7 @@ async function processVerify(state: SurveyState) {
 
     let result: ValidationResult;
     try {
-        result = await state.device.validate();
-        checkEligibility(result);
+        result = await validate(state.device);
     } catch (error) {
         reportError(error);
         result = { status: VerificationState.error };

src/common/alexa/amazon.ts
@@ -441,6 +441,7 @@ async function validateAmazon(): Promise<ValidationResult> {
 }
 
 export const Alexa: Device = {
+    serialize: () => 'alexa',
     accountName: 'Amazon',
     loginURL: upgradeUrl,
     validate: validateAmazon

src/common/device.ts
@@ -1,4 +1,6 @@
 import { ValidationResult } from './types';
+import { Alexa } from './alexa/amazon';
+import { Google } from './google/google';
 
 /**
  * A Device used in our study
@@ -17,4 +19,27 @@ export interface Device {
      * Validate the login status of the current user
      */
     validate(): Promise<ValidationResult>;
+
+    /**
+     * Produce an object representing this device
+     */
+    serialize(): any;
+}
+
+/**
+ * Given a serialized instance of a device, returns the corresponding object
+ *
+ * This relies on us knowing that there are only two devices
+ * and therefore breaks the Device abstractions.
+ * Still, it *is* convenient.
+ */
+export function unserializeDevice(serializedDevice: any): Device {
+    switch (serializedDevice) {
+        case Alexa.serialize():
+            return Alexa;
+        case Google.serialize():
+            return Google;
+        default:
+            throw new Error(`unknown device ${serializedDevice}`);
+    }
 }

commit bbc2485045645e6a7e7f6eb797ccc163c746e2d2

Remove lazy evaluation in GoogleInteractions
This fixes a regression introduced in 1aa529a, when interaction fetching
and processing was moved into the background script. Afterwards,
interactions are sent to the content-script over postMessage, which
implicitly serializes the Interaction objects as JSON. However, since
the GoogleInteraction objects relied on accessors, the object that was
received ended up missing all the important fields.

This commit implements a fix (albeit a slightly hacky one).
It gets rid of the accessors and just sets the fields on the
GoogleInteraction object, so that it will (eventually) be properly
serialized.

src/common/google/google.ts
@@ -225,6 +225,11 @@ class GoogleInteraction implements Interaction {
         return [interactions, errors];
     }
 
+    public readonly recordingAvailable: boolean;
+    public readonly url: string;
+    public readonly transcript: string;
+    public readonly timestamp: number;
+
     private json: any[];
 
     constructor(rawJson: any[]) {
@@ -235,9 +240,8 @@ class GoogleInteraction implements Interaction {
             );
         }
         this.json = rawJson;
-    }
 
-    get transcript() {
+        // Set transcript
         if (this.json.length <= GoogleInteraction.TRANSCRIPT_INDEX) {
             throw new Error(
                 `interaction missing transcript at ${
@@ -249,36 +253,33 @@ class GoogleInteraction implements Interaction {
         ) {
             throw new Error('transcript is not an array');
         }
+        this.transcript = this.json[GoogleInteraction.TRANSCRIPT_INDEX][0];
 
-        return this.json[GoogleInteraction.TRANSCRIPT_INDEX][0];
-    }
-
-    /**
-     * Return true if the interaction contains a recording URL
-     */
-    get recordingAvailable(): boolean {
+        // Set recordingVailable
         if (this.json.length <= GoogleInteraction.URL_INDEX) {
-            return false;
+            this.recordingAvailable = false;
         } else if (!Array.isArray(this.json[GoogleInteraction.URL_INDEX])) {
-            return false;
+            this.recordingAvailable = false;
+        } else {
+            this.recordingAvailable = true;
         }
 
-        return true;
-    }
+        // Set url
+        if (this.recordingAvailable) {
+            if (this.json.length <= GoogleInteraction.URL_INDEX) {
+                throw new Error(
+                    `interaction missing url at ${GoogleInteraction.URL_INDEX}`
+                );
+            } else if (!Array.isArray(this.json[GoogleInteraction.URL_INDEX])) {
+                throw new Error('interaction missing url');
+            }
 
-    get url() {
-        if (this.json.length <= GoogleInteraction.URL_INDEX) {
-            throw new Error(
-                `interaction missing url at ${GoogleInteraction.URL_INDEX}`
-            );
-        } else if (!Array.isArray(this.json[GoogleInteraction.URL_INDEX])) {
-            throw new Error('interaction missing url');
+            this.url = this.json[GoogleInteraction.URL_INDEX][0];
+        } else {
+            this.url = '';
         }
 
-        return this.json[GoogleInteraction.URL_INDEX][0];
-    }
-
-    get timestamp() {
+        // Set timestamp
         if (this.json.length <= GoogleInteraction.TIMESTAMP_INDEX) {
             throw new Error(
                 `interaction missing timestamp at ${
@@ -287,7 +288,9 @@ class GoogleInteraction implements Interaction {
             );
         }
 
-        return parseTimestamp(this.json[GoogleInteraction.TIMESTAMP_INDEX]);
+        this.timestamp = parseTimestamp(
+            this.json[GoogleInteraction.TIMESTAMP_INDEX]
+        );
     }
 
     /**
@@ -348,24 +351,6 @@ function extractData(data: GoogleActivityList): [GoogleInteraction[], Error[]] {
     ];
 }
 
-function validateInteractions(interactions: Interaction[]): Error[] {
-    const errors: Error[] = [];
-    interactions.forEach(interaction => {
-        // The purpose of this loop is to access the fields of the Interaction.
-        // Because the implementation of GoogleInteraction uses getters,
-        // they are lazy-evaluated, so we wouldn't know until we tried
-        // accessing them whether they're valid or not.
-        // We want to know that *now* rather than later,
-        // so we try accessing them now.
-        try {
-            const { transcript, timestamp } = interaction;
-        } catch (error) {
-            errors.push(error);
-        }
-    });
-    return errors;
-}
-
 /**
  * Validate Home user status and eligibility and get interaction data
  *
@@ -384,9 +369,8 @@ async function validateGoogle(): Promise<ValidationResult> {
 
     // Extract interactions from the data
     const [interactions, extractErrors] = extractData(activities);
-    const validationErrors = validateInteractions(interactions);
 
-    const errors = downloadErrors.concat(extractErrors, validationErrors);
+    const errors = downloadErrors.concat(extractErrors);
 
     return { status: VerificationState.loggedIn, interactions, errors };
 }