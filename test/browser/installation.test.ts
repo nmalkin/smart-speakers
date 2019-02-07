902564fc8ebb0b88f6572b1029541732b624e541
Nathan Malkin <nmalkin@cs.berkeley.edu>
2019-02-07 15:46:57 -0800

    Increase test timeout

--- test/browser/installation.test.ts
+++ test/browser/installation.test.ts
@@ -29,5 +29,5 @@ describe('test installation', () => {
         const browser = await getBrowser();
         expect(browser).toBeTruthy();
         await browser.close();
-    }, 5000);
+    }, 30000);
 });
